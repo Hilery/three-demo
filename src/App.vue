5a8cad119928bc799633707bf8653d06f5288a6e
magic_night_zlm <zhu45712@gmail.com>
2022-07-26 20:47:09 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+	  <div class="column">
+		  <gltf-viewer url="./models/touzi/scene.gltf"></gltf-viewer>
+	  </div>		
+  </div>
+</template>
+
+<script>
+import GltfViewer from './components/GltfViewer';
+
+export default {
+	name: 'app',
+	components: {
+		GltfViewer,
+	}
+}
+</script>
+
+<style>
+.column {
+	width: 100%;
+	height: 100vh;
+}
+</style>
---
+++ src/components/GltfViewer.vue
@@ -0,0 +1,33 @@
+<template>
+    <div></div>
+</template>
+<script>
+
+export default {
+	props: ['url'],
+	mounted() {
+		const Viewr = require('../../lib/viewer');
+		const dat = require('dat.gui');
+		this.viewer = new Viewr(this.$el, {});
+		dat.GUI.toggleHide();
+		if (this.url) {
+			this.viewer.load(this.url);
+		}
+	},
+	data() {
+		return {
+			viewer: null,
+		};
+	}
+}
+</script>
+<style scoped>
+div {
+	min-height: 512px;
+	min-width: 512px;
+	width: 100%;
+	height: 100%;
+}
+</style>
+
+
