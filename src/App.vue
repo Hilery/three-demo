722f8bf934346e4fb13097be24cfd70701ec5290
黎明 <306567577@qq.com>
2022-07-27 10:30:43 +0800

    更新名字

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
   <div id="app">
     <div class="left">
-      <el-menu
-        default-active="2"
-        class="el-menu-vertical-demo"
-        @select="handleSelect"
-      >
+      <el-menu class="el-menu-vertical-demo" @select="handleSelect">
         <el-menu-item index="1">
           <i class="el-icon-location"></i>
-          <span slot="title">导航一</span>
+          <span slot="title">房子</span>
         </el-menu-item>
         <el-menu-item index="2">
           <i class="el-icon-menu"></i>
-          <span slot="title">导航二</span>
+          <span slot="title">汽水</span>
         </el-menu-item>
         <el-menu-item index="3">
           <i class="el-icon-document"></i>
-          <span slot="title">导航三</span>
+          <span slot="title">锤子</span>
         </el-menu-item>
         <el-menu-item index="4">
           <i class="el-icon-setting"></i>
-          <span slot="title">导航四</span>
+          <span slot="title">骰子</span>
         </el-menu-item>
       </el-menu>
     </div>
